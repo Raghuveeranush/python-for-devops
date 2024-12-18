7cf0a2b78b33cf73127f77a9adb6efa675fd4573
Busetty Raghuveer Anush <Raghuveer.anush316@gmail.com>
2024-12-18 06:07:19 +0000

    made some chgs

--- Day-02/examples/test.py
+++ Day-02/examples/test.py
@@ -1,12 +1,7 @@
 arn = "arn:aws:ec2:us-east-1:123456789012:vpc/vpc-0e9801d129EXAMPLE"
-str = "Busetty"
-str1 = "Jaanav"
 
-full_name = str+" "+str1
-
-print ("My Name is:", full_name)
-
-print (arn.split("/")[1])
+vpc = (arn.split("/")[1])
+print("vpc id:",vpc)
 
 str_length = len(arn.split("/")[1])
 
